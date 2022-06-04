165c603e26a30d5de8c90d542c81065cfbabaa40
vnknowledge <6498866+vnknowledge2014@users.noreply.github.com>
2022-06-04 23:21:10 +0700

    Delete server/dist directory

--- server/dist/controllers/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.searchDrug = exports.drugCrawler = void 0;
-var crawler_1 = require("./crawler");
-Object.defineProperty(exports, "drugCrawler", { enumerable: true, get: function () { return crawler_1.drugCrawler; } });
-var search_1 = require("./search");
-Object.defineProperty(exports, "searchDrug", { enumerable: true, get: function () { return search_1.searchDrug; } });
-//# sourceMappingURL=index.js.map
